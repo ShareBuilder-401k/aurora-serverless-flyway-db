5f13756941cb51463ae52b22141bab76970f17f3
Michael Reed <michaelr@sb401k.com>
2020-04-24 14:05:58 -0700

    Add initial workflow and action setup for generate markdown

---
+++ .github/actions/generate-markdown-action/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo 'Testing to see where checked out files show up here'
+
+ls
+
+# FLWAY_OPTIONS=(
+#   -url=jdbc:postgresql://postgres:5432/$POSTGRES_DB
+#   -user=$POSTGRES_USER
+#   -password=$POSTGRES_PASSWORD
+#   -locations=
+# )
