7d6a742048736cf27598d27fbfda6b4d80005232
Michael Reed <michaelr@sb401k.com>
2020-04-24 14:56:11 -0700

    See what is being passed to the action

--- .github/actions/generate-markdown-action/entrypoint.sh
+++ .github/actions/generate-markdown-action/entrypoint.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
-FLWAY_OPTIONS=(
-  -url=jdbc:postgresql://postgres:5432/$POSTGRES_DB
-  -user=$POSTGRES_USER
-  -password=$POSTGRES_PASSWORD
-  -locations=filesystem:./sql
-  -placeholders.DATABASE_NAME=$POSTGRES_DB
-  -placeholders.READ_ONLY_PASSWORD=password
-  -placeholders.SAMPLE_APPLICATION_PASSWORD=password
-)
+echo $POSTGRES_DB
+echo $POSTGRES_USER
+echo $POSTGRES_PASSWORD
 
-/flyway/flyway "${FLYWAY_OPTIONS[@]}" info
-/flyway/flyway "${FLYWAY_OPTIONS[@]}" migrate
-/flyway/flyway "${FLYWAY_OPTIONS[@]}" info
+# FLWAY_OPTIONS=(
+#   -url=jdbc:postgresql://postgres:5432/$POSTGRES_DB
+#   -user=$POSTGRES_USER
+#   -password=$POSTGRES_PASSWORD
+#   -locations=filesystem:./sql
+#   -placeholders.DATABASE_NAME=$POSTGRES_DB
+#   -placeholders.READ_ONLY_PASSWORD=password
+#   -placeholders.SAMPLE_APPLICATION_PASSWORD=password
+# )
 
-postgres-markdown -H postgres --user $POSTGRES_USER --password $POSTGRES_PASSWORD --database $POSTGRES_DB -l en --output postgres.md
+# /flyway/flyway "${FLYWAY_OPTIONS[@]}" info
+# /flyway/flyway "${FLYWAY_OPTIONS[@]}" migrate
+# /flyway/flyway "${FLYWAY_OPTIONS[@]}" info
 
-sed -i '/#Database Documentation/,$d' README.md
-cat postgres.md >> README.md
+# postgres-markdown -H postgres --user $POSTGRES_USER --password $POSTGRES_PASSWORD --database $POSTGRES_DB -l en --output postgres.md
 
+# sed -i '/#Database Documentation/,$d' README.md
+# cat postgres.md >> README.md
 
-git config user.email \"engineering@sb401k.com\"
-git config user.name \"sb401k-gitbot\"
-git add README.md
-git commit -m 'Updating Postgres Markdown on README'
-git push https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+# git config user.email \"engineering@sb401k.com\"
+# git config user.name \"sb401k-gitbot\"
+# git add README.md
+# git commit -m 'Updating Postgres Markdown on README'
+# git push https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
