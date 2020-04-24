f30798c61117e4ca7e5dc334a796ab4edfc1f566
Michael Reed <michaelr@sb401k.com>
2020-04-24 14:59:58 -0700

    Found the typo

--- .github/actions/generate-markdown-action/entrypoint.sh
+++ .github/actions/generate-markdown-action/entrypoint.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
 
-echo $POSTGRES_DB
-echo $POSTGRES_USER
-echo $POSTGRES_PASSWORD
+FLYWAY_OPTIONS=(
+  -url=jdbc:postgresql://postgres:5432/$POSTGRES_DB
+  -user=$POSTGRES_USER
+  -password=$POSTGRES_PASSWORD
+  -locations=filesystem:./sql
+  -placeholders.DATABASE_NAME=$POSTGRES_DB
+  -placeholders.READ_ONLY_PASSWORD=password
+  -placeholders.SAMPLE_APPLICATION_PASSWORD=password
+)
 
-# FLWAY_OPTIONS=(
-#   -url=jdbc:postgresql://postgres:5432/$POSTGRES_DB
-#   -user=$POSTGRES_USER
-#   -password=$POSTGRES_PASSWORD
-#   -locations=filesystem:./sql
-#   -placeholders.DATABASE_NAME=$POSTGRES_DB
-#   -placeholders.READ_ONLY_PASSWORD=password
-#   -placeholders.SAMPLE_APPLICATION_PASSWORD=password
-# )
+/flyway/flyway "${FLYWAY_OPTIONS[@]}" info
+/flyway/flyway "${FLYWAY_OPTIONS[@]}" migrate
+/flyway/flyway "${FLYWAY_OPTIONS[@]}" info
 
-# /flyway/flyway "${FLYWAY_OPTIONS[@]}" info
-# /flyway/flyway "${FLYWAY_OPTIONS[@]}" migrate
-# /flyway/flyway "${FLYWAY_OPTIONS[@]}" info
+postgres-markdown -H postgres --user $POSTGRES_USER --password $POSTGRES_PASSWORD --database $POSTGRES_DB -l en --output postgres.md
 
-# postgres-markdown -H postgres --user $POSTGRES_USER --password $POSTGRES_PASSWORD --database $POSTGRES_DB -l en --output postgres.md
+sed -i '/#Database Documentation/,$d' README.md
+cat postgres.md >> README.md
 
-# sed -i '/#Database Documentation/,$d' README.md
-# cat postgres.md >> README.md
-
-# git config user.email \"engineering@sb401k.com\"
-# git config user.name \"sb401k-gitbot\"
-# git add README.md
-# git commit -m 'Updating Postgres Markdown on README'
-# git push https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+git config user.email \"engineering@sb401k.com\"
+git config user.name \"sb401k-gitbot\"
+git add README.md
+git commit -m 'Updating Postgres Markdown on README'
+git push https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
