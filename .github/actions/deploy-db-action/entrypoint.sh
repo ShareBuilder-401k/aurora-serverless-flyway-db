d5a3969572c2315c72a9db2ae073671135161d26
Michael Reed <michaelr@sb401k.com>
2020-04-26 16:19:38 -0700

    Try to output job level env vars in actions

--- .github/actions/deploy-db-action/entrypoint.sh
+++ .github/actions/deploy-db-action/entrypoint.sh
@@ -8,4 +8,8 @@ echo $REGION
 echo "Testing setting env vars in workflow persists to action"
 echo "AWS env vars should be set so this should output something here"
 
+echo $AWS_DEFAULT_REGION
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+
 aws s3 ls
