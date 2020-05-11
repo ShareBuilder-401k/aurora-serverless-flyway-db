3fbafd4bca7293b7334abfd09f16ef3b80ad4679
Michael Reed <michaelr@sb401k.com>
2020-05-11 09:38:12 -0700

    Why isn't region being passed?

--- .github/actions/restore-snapshot-action/entrypoint.sh
+++ .github/actions/restore-snapshot-action/entrypoint.sh
@@ -8,6 +8,8 @@ db_subnet_group=$3
 vpc_security_groups=$4
 region=$5
 
+echo $region
+
 scaling_configuration="MinCapacity=2,MaxCapacity=16,AutoPause=true,SecondsUntilAutoPause=300,TimeoutAction=ForceApplyCapacityChange"
 
 RestoreClusterSnapshot $db_cluster $snapshot_name $db_subnet_group $vpc_security_groups $scaling_configuration $region
