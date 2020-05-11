5ccfb5b159c5ef56a7c61e80a0286c16bbd2b9f0
Michael Reed <michaelr@sb401k.com>
2020-05-11 09:16:27 -0700

    Fix variable names in restore snapshot

--- .github/actions/restore-snapshot-action/entrypoint.sh
+++ .github/actions/restore-snapshot-action/entrypoint.sh
@@ -10,4 +10,4 @@ region=$5
 
 scaling_configuration="MinCapacity=2,MaxCapacity=16,AutoPause=true,SecondsUntilAutoPause=300,TimeoutAction=ForceApplyCapacityChange"
 
-RestoreClusterSnapshot $DB_CLUSTER $SNAPSHOT_NAME $DB_SUBNET_GROUP $VPC_SECURITY_GROUPS $SCALING_CONFIGURATION $REGION
+RestoreClusterSnapshot $db_cluster $snapshot_name $db_subnet_group $vpc_security_groups $scaling_configuration $region
