6dbf074776d813ffeb88f50debaa8044bd35376d
Michael Reed <michaelr@sb401k.com>
2020-09-28 14:00:20 -0700

    Rename some variables

--- .github/actions/restore-snapshot-action/entrypoint.sh
+++ .github/actions/restore-snapshot-action/entrypoint.sh
@@ -2,12 +2,12 @@
 
 source /functions.sh
 
-db_cluster=$1
-snapshot_name=$2
+db_cluster_name=$1
+db_snapshot_name=$2
 db_subnet_group=$3
 vpc_security_groups=$4
-region=$5
+db_cluster_region=$5
 
 scaling_configuration="MinCapacity=2,MaxCapacity=16,AutoPause=true,SecondsUntilAutoPause=300,TimeoutAction=ForceApplyCapacityChange"
 
-RestoreClusterSnapshot $db_cluster $snapshot_name $db_subnet_group $vpc_security_groups $scaling_configuration $region
+RestoreClusterSnapshot $db_cluster_name $db_snapshot_name $db_subnet_group $vpc_security_groups $scaling_configuration $db_cluster_region
